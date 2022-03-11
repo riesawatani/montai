0cc3bb2e687a6b04132fe3271ce3c95386b0f592
Rie Sawatani <rie@sawatani.jp>
2022-03-11 20:33:36 +0900

    赤い牛

--- main.go
+++ main.go
@@ -1,22 +1,69 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
+	"github.com/hajimehoshi/ebiten/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/text"
+	"github.com/riesawatani/montai/niku"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
 )
 
+var (
+	mPlus1pRegular_ttf font.Face
+)
+
+func init() {
+	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ft, err := opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    24,
+		DPI:     72,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	mPlus1pRegular_ttf = ft
+}
+
 type Game struct {
-	niku Niku
+	Msg   string
+	Count int
+	niku  niku.Niku
 }
 
 func (g *Game) Update() error {
+	g.Count = g.Count + 1
+	if g.Count < 60 {
+		return nil
+	}
+	g.Count = 0
+	if len(g.Msg) > 0 {
+		g.Msg = ""
+	} else {
+		g.Msg = g.niku.Taberu()
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, g.niku.Neme)
+	x := 90
+	y := 120 + g.Count
+
+	iro := color.RGBA{
+		R: 255,
+		G: 0,
+		B: uint8(4 * g.Count),
+		A: 100,
+	}
+
+	text.Draw(screen, g.Msg, mPlus1pRegular_ttf, x, y, iro)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -27,18 +74,21 @@ func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
 
+	buta := niku.Niku{
+		Neme: "buta",
+	}
+	log.Println(buta)
+
+	ushi := niku.Niku{
+		Neme: "ushi",
+	}
+	log.Println(ushi)
+
 	game := &Game{
-		niku: Niku{
-			Neme: "FFFFFFFF",
-		},
+		niku: ushi,
 	}
 
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
-}
-
-type Niku struct {
-	Neme string
-	sizu int
 }
--- niku/niku.go
+++ niku/niku.go
@@ -1,4 +1,10 @@
 package niku
 
 type Niku struct {
+	Neme string
+	Size int
+}
+
+func (a *Niku) Taberu() string {
+	return a.Neme
 }
