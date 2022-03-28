58e88b0ade490774e64ae295821c5b223608b27e
Rie Sawatani <rie@sawatani.jp>
2022-03-28 19:57:42 +0900

    fzisann

--- qstrage/qup.go
+++ qstrage/qup.go
@@ -2,7 +2,10 @@ package qstrage
 
 import (
 	"context"
+	"io"
+	"log"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
@@ -16,4 +19,21 @@ func GetClient(ctx context.Context) (*s3.Client, error) {
 	}
 	client := s3.NewFromConfig(cfg)
 	return client, nil
+}
+
+func writefile(ctx context.Context, s3path string, content io.Reader) error {
+	log.Printf("write S3 file: %s/%s", Bucketname, s3path)
+	client, err := GetClient(ctx)
+	if err != nil {
+		return err
+	}
+	obj := &s3.PutObjectInput{
+		Bucket: aws.String(Bucketname),
+		Key:    aws.String(s3path),
+		Body:   content,
+	}
+	if _, err := client.PutObject(ctx, obj); err != nil {
+		return err
+	}
+	return nil
 }
--- tyoko/tyoko.go
+++ tyoko/tyoko.go
@@ -7,15 +7,27 @@ type Tyoko struct {
 
 var Xlist = []Tyoko{
 	{
-		Question: "6 + 3 =",
-		Answer:   "9",
+		Question: "池=K 石=C 家=A 岩=?",
+		Answer:   "Y",
 	},
 	{
-		Question: "5 + 3 =",
-		Answer:   "8",
+		Question: "正解！！",
+		Answer:   "Space",
 	},
 	{
-		Question: "4 - 3 =",
+		Question: "10-9=1 10-5=1 10-3=?",
+		Answer:   "3",
+	},
+	{
+		Question: "正解！！",
+		Answer:   "Space",
+	},
+	{
+		Question: "4-3=",
 		Answer:   "1",
+	},
+	{
+		Question: "正解！！",
+		Answer:   "Space",
 	},
 }
